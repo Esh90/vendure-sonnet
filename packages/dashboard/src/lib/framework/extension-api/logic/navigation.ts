dee5a33814b7b1fc2d7339ecfab1b84435633086
Michael Bromley <michael@michaelbromley.co.uk>
2026-03-11 10:49:15 +0100

    refactor(dashboard): Reduce cognitive complexity of registerNavigationExtensions

--- packages/dashboard/src/lib/framework/extension-api/logic/navigation.ts
+++ packages/dashboard/src/lib/framework/extension-api/logic/navigation.ts
@@ -11,44 +11,49 @@ export function registerNavigationExtensions(
     navSections?: DashboardNavSectionDefinition[] | ((config: NavMenuConfig) => NavMenuConfig),
     routes?: DashboardRouteDefinition[],
 ): ((config: NavMenuConfig) => NavMenuConfig) | undefined {
-    let navMenuModifier: ((config: NavMenuConfig) => NavMenuConfig) | undefined;
-
-    if (navSections) {
-        if (typeof navSections === 'function') {
-            navMenuModifier = navSections;
-        } else {
-            for (const section of navSections) {
-                addNavMenuSection({
-                    ...section,
-                    placement: section.placement ?? 'top',
-                    order: section.order ?? 999,
-                    items: [],
-                });
-            }
-        }
-    }
-
-    if (routes) {
-        for (const route of routes) {
-            if (route.navMenuItem) {
-                // Add the nav menu item
-                const item: NavMenuItem = {
-                    url: route.navMenuItem.url ?? route.path,
-                    id: route.navMenuItem.id ?? route.path,
-                    title: route.navMenuItem.title ?? route.path,
-                    order: route.navMenuItem.order,
-                    requiresPermission: route.navMenuItem.requiresPermission,
-                    icon: route.navMenuItem.icon,
-                    placement: route.navMenuItem.placement,
-                };
-                addNavMenuItem(item, route.navMenuItem.sectionId);
-            }
-            if (route.path) {
-                // Configure a list page
-                registerRoute(route);
-            }
-        }
-    }
-
+    const navMenuModifier = registerNavSections(navSections);
+    registerRoutes(routes);
     return navMenuModifier;
+}
+
+function registerNavSections(
+    navSections?: DashboardNavSectionDefinition[] | ((config: NavMenuConfig) => NavMenuConfig),
+): ((config: NavMenuConfig) => NavMenuConfig) | undefined {
+    if (!navSections) {
+        return;
+    }
+    if (typeof navSections === 'function') {
+        return navSections;
+    }
+    for (const section of navSections) {
+        addNavMenuSection({
+            ...section,
+            placement: section.placement ?? 'top',
+            order: section.order ?? 999,
+            items: [],
+        });
+    }
+}
+
+function registerRoutes(routes?: DashboardRouteDefinition[]) {
+    if (!routes) {
+        return;
+    }
+    for (const route of routes) {
+        if (route.navMenuItem) {
+            const item: NavMenuItem = {
+                url: route.navMenuItem.url ?? route.path,
+                id: route.navMenuItem.id ?? route.path,
+                title: route.navMenuItem.title ?? route.path,
+                order: route.navMenuItem.order,
+                requiresPermission: route.navMenuItem.requiresPermission,
+                icon: route.navMenuItem.icon,
+                placement: route.navMenuItem.placement,
+            };
+            addNavMenuItem(item, route.navMenuItem.sectionId);
+        }
+        if (route.path) {
+            registerRoute(route);
+        }
+    }
 }
